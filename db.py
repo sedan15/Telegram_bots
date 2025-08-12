590edafaccfb1040a5a8db7093b082c7a51c7ced
Envelope <nikolajrodin905@gmail.com>
2025-08-12 19:19:56 +0400

    db work

--- db.py
+++ db.py
@@ -11,6 +11,21 @@ db = mysql.connector.connect(
 db.autocommit = True #Свойство автоотправки запросов insert
 cursor = db.cursor() #С помощью этой переменной будем делать запросы к бд
 
+#Получить 1 запись по id
+def get_one_by_id(table:str,id:int):
+    query = f'''SELECT * FROM {table} WHERE id = {id}'''
+    cursor.execute(query)
+    record = cursor.fetchone() #Получение результатов запроса
+    return record
+
+#Сохранить 1 запись
+def save_one(table:str,record:str,values:str):
+    query = f'''INSERT INTO {table}({record}) VALUES({values})'''
+    cursor.execute(query)
+#Удалить 1 запись по id
+def delete_one_by_id(table:str,id:int):
+    query = f'''DELETE FROM {table} WHERE id = {id}'''
+    cursor.execute(query)
 
 # cursor.execute('SELECT * FROM users;') #Сам запрос
 # result = cursor.fetchone() #Обработка запроса
